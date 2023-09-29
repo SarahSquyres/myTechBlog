7c476ac52134e7a41aa1bef2800a79d14985a619
Sarah Squyres <ssquyres15@gmail.com>
2023-09-29 16:14:51 -0500

    setting up user and post routes, models and main/homepage handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {
+    User,
+    Post,
+    Comment
+} = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({
+                plain: true
+            }));
+
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
+
+router.get('*', (req, res) => {
+    res.status(404).send("I think something went wrong");
+})
+
+module.exports = router;
