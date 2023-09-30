ea0282cfbefff536a18ad1bf409536ec660aa508
Sarah Squyres <ssquyres15@gmail.com>
2023-09-30 12:29:48 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-  dialect: 'mysql',
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
   }
 );
 
